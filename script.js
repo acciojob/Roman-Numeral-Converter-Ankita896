7278cb968fa0a3f4eb97c63d717e7365fec73f9b
Ankita896 <ankita143ankyy@gmail.com>
2025-06-03 20:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,20 +9,39 @@ function convertToRoman(num) {
       6:['I', 1]
     ];
 
-  let result = '';
+ let result = '';
 
-    let i=0;
-	while(num>0){
-		
-		let val=obj[i][1];
-		for(;num>=val; num-=val{
-			result+=obj[i][0];
-		}
-		i++;
-	}
-    
+    for (let i = 0; i < obj.length; i++) {
+        const [symbol, value] = obj[i];
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    // Handle cases like 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD), 900 (CM)
+    const specialCases = [
+        ['CM', 900],
+        ['CD', 400],
+        ['XC', 90],
+        ['XL', 40],
+        ['IX', 9],
+        ['IV', 4]
+    ];
+
+    for (let i = 0; i < specialCases.length; i++) {
+        const [symbol, value] = specialCases[i];
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
 
     return result;
+
+    
+
+   
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
