4001f951eb01129dcefee399a584e25e808b8087
Ankita896 <ankita143ankyy@gmail.com>
2025-08-04 23:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 function convertToRoman(num) {
-  	const obj = [
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    ];
+  const obj = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-   let result = '';
+  let result = '';
 
-   for (let i = 0; i < obj.length; i++) {
-        while (num >= obj[i][1]) {
-            result += obj[i][0]; 
-            num -= obj[i][1]; 
-        }
+  for (let i = 0; i < obj.length; i++) {
+    while (num >= obj[i][1]) {
+      result += obj[i][0];
+      num -= obj[i][1];
     }
-    
-    return result;
+  }
+
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+function handleConvert() {
+  const input = document.getElementById("numberInput").value;
+  const output = document.getElementById("output");
 
+  const number = parseInt(input);
 
+  if (isNaN(number) || number < 1 || number > 100000) {
+    output.textContent = "Please enter a number between 1 and 100000.";
+    return;
+  }
 
+  output.textContent = `Roman Numeral: ${convertToRoman(number)}`;
+}
 
-// do not edit below this line
-module.exports = convertToRoman
+// Do not edit below this line for test compatibility
+if (typeof module !== 'undefined') {
+  module.exports = convertToRoman;
+}
